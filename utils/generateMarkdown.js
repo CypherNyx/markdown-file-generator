e5536209710b98a99c69f915ecbd02cffadec741
Dahlia Guido <dguido.dev@gmail.com>
2023-07-21 23:46:00 -0500

    adding the license badges and links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,35 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  if(license.length === 0){
+    return ''
+  } else if (license == 'MIT'){
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+
+  } else if (license == 'Mozilla'){
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+  } else if(license == 'GNU'){
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  } else if (license == 'Apache'){
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
 };
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license.length === 0){
+    return ''
+  } else if (license == 'MIT'){
+    return 'https://opensource.org/licenses/MIT'
+  } else if (license == 'Mozilla'){
+    return 'https://opensource.org/licenses/MPL-2.0'
+  } else if(license == 'GNU'){
+    return 'https://www.gnu.org/licenses/gpl-3.0'
+  } else if (license == 'Apache'){
+    return 'https://opensource.org/licenses/Apache-2.0'
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
