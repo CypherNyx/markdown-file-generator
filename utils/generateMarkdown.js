ec2d4ccce45d7ca0b0548e47177d18c325e29693
Dahlia Guido <dguido.dev@gmail.com>
2023-07-22 00:05:14 -0500

    write markdown section inside string template literals

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(license.length === 0){
@@ -31,13 +31,53 @@ function renderLicenseLink(license) {
   }
 };
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license.length === 0){
+    return ''
+  } else {
+    return `## License
+    ${renderLicenseBadge(license)}
 
-// TODO: Create a function to generate markdown for README
+    ${renderLicenseLink(license)}
+    `
+  }
+};
+
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Questions](#questions)
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## Tests
+  ${data.test}
+
+  ## Contributing
+  ${data.contribute}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Questions
+  GitHub [${data.username}](https://github.com/${data.username})
+  Email: ${data.email}
 
 `;
 }
