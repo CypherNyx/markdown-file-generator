dac92c872f6f26dc099c7687fa448a12e0c9c79f
Dahlia Guido <dguido.dev@gmail.com>
2023-07-21 23:16:42 -0500

    prompt questions are working

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license.length === 0){
-    return ''
-  } else if 
 };
 
 // TODO: Create a function that returns the license link
